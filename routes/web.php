27675ec48c477890fe5a258a1267d6c7d4369574
Камалиев Робин <robinkamaliev@mail.ru>
2025-05-19 23:16:06 +0300

    добавлены ендпоинты

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('/admin/news', [AdminController::class, 'news'])->name('admin.news');
+Route::get('/admin/news/{id}', [AdminController::class, 'show'])->name('admin.news.show');
+
+Route::middleware('auth')->group(static function (): void {
+    Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/news/search', [NewsController::class, 'search'])->name('news.search');
+    Route::get('/news/parser', [NewsController::class, 'parser'])->name('news.parser');
 });
+
+Route::get('/login', fn () => 'Please login')->name('login'); //todo: заглушка
